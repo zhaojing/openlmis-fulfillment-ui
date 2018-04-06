1421a730acc47f804f17aec0d51531f5dc20a7e4
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2018-04-06 13:26:07 +0200

    OLMIS-4391: Refactored test for routes

--- src/order-fulfillment/order-fulfillment.routes.spec.js
+++ src/order-fulfillment/order-fulfillment.routes.spec.js
@@ -16,87 +16,25 @@
 
 describe('openlmis.orders.fulfillment state', function() {
 
-    var $q, $state, $rootScope, requestingFacilityFactory, state, minimalFacilities,
-        FULFILLMENT_RIGHTS, $stateParams, facilityFactory, facilities, paginationService,
-        orderRepository, ORDER_STATUS, programs, programService, MinimalFacilityDataBuilder,
-        FacilityDataBuilder, ProgramDataBuilder;
+    var $q, $state, state, $rootScope, requestingFacilityFactory, minimalFacilities,
+        FULFILLMENT_RIGHTS, $stateParams, facilityFactory, facilities, orderRepository,
+        ORDER_STATUS, programs, programService, MinimalFacilityDataBuilder, FacilityDataBuilder,
+        ProgramDataBuilder, paginationService;
 
     beforeEach(function() {
-        loadModules();
-        injectServices();
-        prepareTestData();
-        prepareSpies();
-    });
-
-    it('should fetch a list of ordering facilities', function() {
-        $stateParams.requestingFacilityId = 'requesting-facility';
-
-        var result;
-
-        state.resolve.orderingFacilities(facilities, requestingFacilityFactory)
-        .then(function(orderingFacilities){
-            result = orderingFacilities;
-        });
-        $rootScope.$apply();
-
-        expect(result).toEqual(minimalFacilities);
-    });
-
-    it('should fetch a list of supervised facilities', function() {
-        var result;
-
-        state.resolve.supervisedFacilities(facilityFactory).then(function(supervisedFacilities){
-            result = supervisedFacilities;
-        });
-        $rootScope.$apply();
-
-        expect(result).toEqual(facilities);
-    });
-
-    it('should set FULFILLING and ORDERED statuses as default', function() {
-        $stateParams.status = undefined;
-
-        state.resolve.orders(paginationService, orderRepository, $stateParams, ORDER_STATUS);
-        $rootScope.$apply();
-
-        expect($stateParams.status).toEqual(undefined);
-        expect(orderRepository.search).toHaveBeenCalledWith(
-            {status: [ORDER_STATUS.FULFILLING, ORDER_STATUS.ORDERED], page: 0, size: 10});
-    });
-
-    it('should remove not available statuses from the url', function() {
-        $stateParams.status = [ORDER_STATUS.IN_ROUTE, ORDER_STATUS.ORDERED];
-
-        state.resolve.orders(paginationService, orderRepository, $stateParams, ORDER_STATUS);
-        $rootScope.$apply();
-
-        expect($stateParams.status).toEqual([ORDER_STATUS.ORDERED]);
-        expect(orderRepository.search).toHaveBeenCalledWith(
-            {status: [ORDER_STATUS.ORDERED], page: 0, size: 10});
-    });
-
-    it('should require FULFILLMENT_RIGHTS right to enter', function() {
-        expect(state.accessRights).toEqual(
-            [FULFILLMENT_RIGHTS.SHIPMENTS_VIEW, FULFILLMENT_RIGHTS.ORDERS_VIEW]);
-    });
-
-    function loadModules() {
-        module('openlmis-main-state');
         module('order');
         module('referencedata-facilities-permissions');
         module('referencedata-program');
         module('order-fulfillment');
-    }
 
-    function injectServices() {
         inject(function($injector) {
             $q = $injector.get('$q');
             $state = $injector.get('$state');
             $rootScope = $injector.get('$rootScope');
             facilityFactory = $injector.get('facilityFactory');
             requestingFacilityFactory = $injector.get('requestingFacilityFactory');
-            paginationService = $injector.get('paginationService');
             programService = $injector.get('programService');
+            paginationService = $injector.get('paginationService');
             orderRepository = $injector.get('orderRepository');
             ORDER_STATUS = $injector.get('ORDER_STATUS');
             FULFILLMENT_RIGHTS = $injector.get('FULFILLMENT_RIGHTS');
@@ -104,11 +42,16 @@ describe('openlmis.orders.fulfillment state', function() {
             FacilityDataBuilder = $injector.get('FacilityDataBuilder');
             ProgramDataBuilder = $injector.get('ProgramDataBuilder');
         });
-    }
 
-    function prepareTestData() {
-        state = $state.get('openlmis.orders.fulfillment');
-        $stateParams = {};
+        $state.go('openlmis');
+        $rootScope.$apply();
+
+        $stateParams = {
+            status: undefined,
+            page: 0,
+            size: 10
+        };
+
         facilities = [
             new FacilityDataBuilder().withId('facility-1').build(),
             new FacilityDataBuilder().withId('facility-2').build()
@@ -122,12 +65,74 @@ describe('openlmis.orders.fulfillment state', function() {
             new ProgramDataBuilder().build(),
             new ProgramDataBuilder().build()
         ];
-    }
 
-    function prepareSpies() {
+        spyOn(paginationService, 'registerUrl').andCallFake(function(stateParams, method) {
+            return method(stateParams);
+        });
+
         spyOn(facilityFactory, 'getSupervisedFacilitiesBasedOnRights').andReturn($q.when(facilities));
         spyOn(requestingFacilityFactory, 'loadRequestingFacilities').andReturn($q.when(minimalFacilities));
-        spyOn(orderRepository, 'search').andReturn($q.when());
+        spyOn(orderRepository, 'search').andReturn($q.when([]));
         spyOn(programService, 'getAll').andReturn($q.when(programs));
+    });
+
+    it('should fetch a list of ordering facilities', function() {
+        goToState();
+
+        expect(getResolvedValue('orderingFacilities')).toEqual(minimalFacilities);
+    });
+
+    it('should fetch a list of supervised facilities', function() {
+        goToState();
+        expect(getResolvedValue('supervisedFacilities')).toEqual(facilities);
+    });
+
+    it('should fetch a list of programs', function() {
+        goToState();
+
+        expect(getResolvedValue('programs')).toEqual(programs);
+    });
+
+    it('should set FULFILLING and ORDERED statuses as default', function() {
+        goToState();
+
+        expect(orderRepository.search).toHaveBeenCalledWith({
+            requestingFacilityId: undefined,
+            programId: undefined,
+            status: [ORDER_STATUS.FULFILLING, ORDER_STATUS.ORDERED],
+            page: '0',
+            size: '10',
+            sort: 'createdDate,desc'
+        });
+    });
+
+    it('should remove not available statuses from the url', function() {
+        $stateParams.status = [ORDER_STATUS.IN_ROUTE, ORDER_STATUS.ORDERED];
+
+        goToState();
+
+        expect(orderRepository.search).toHaveBeenCalledWith({
+            requestingFacilityId: undefined,
+            programId: undefined,
+            status: [ORDER_STATUS.ORDERED],
+            page: '0',
+            size: '10',
+            sort: 'createdDate,desc'
+        });
+    });
+
+    it('should require FULFILLMENT_RIGHTS right to enter', function() {
+        state = $state.get('openlmis.orders.fulfillment');
+        expect(state.accessRights).toEqual(
+            [FULFILLMENT_RIGHTS.SHIPMENTS_VIEW, FULFILLMENT_RIGHTS.ORDERS_VIEW]);
+    });
+
+    function goToState() {
+        $state.go('openlmis.orders.fulfillment', $stateParams);
+        $rootScope.$apply();
+    }
+
+    function getResolvedValue(name) {
+        return $state.$current.locals.globals[name];
     }
 });
